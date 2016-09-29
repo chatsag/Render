9d2aa34252d9ef3090ada54ffb058542ac52084e
Sagnik Chatterjee <sagnik.chatterjee95@gmail.com>
2016-09-29 19:05:39 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,31 +3,31 @@ $(document).ready(function(){
 	document.querySelector("#HTMLText").addEventListener('keydown',function(e) {
 		if(e.keyCode === 9) { // tab was pressed
 	    	// get caret position/selection
-	    	var start = this.selectionStart;
-	    	var end = this.selectionEnd;
+	    		var start = this.selectionStart;
+	    		var end = this.selectionEnd;
 			var target = e.target;
 			var value = target.value;
 			// set textarea value to: text before caret + tab + text after caret
-	    	target.value = value.substring(0, start)+ "\t"+ value.substring(end);
+	    		target.value = value.substring(0, start)+ "\t"+ value.substring(end);
 			// put caret at right position again (add one for the tab)
-	    	this.selectionStart = this.selectionEnd = start + 1;
+	    		this.selectionStart = this.selectionEnd = start + 1;
 			// prevent the focus lose
-	    	e.preventDefault();
+	    		e.preventDefault();
 	   	}	
 	},false);
 	document.querySelector("#CSSText").addEventListener('keydown',function(e) {
 		if(e.keyCode === 9) { // tab was pressed
 	    	// get caret position/selection
-	    	var start = this.selectionStart;
-	    	var end = this.selectionEnd;
+	    		var start = this.selectionStart;
+	    		var end = this.selectionEnd;
 			var target = e.target;
 			var value = target.value;
 			// set textarea value to: text before caret + tab + text after caret
-	    	target.value = value.substring(0, start)+ "\t"+ value.substring(end);
+	    		target.value = value.substring(0, start)+ "\t"+ value.substring(end);
 			// put caret at right position again (add one for the tab)
-	    	this.selectionStart = this.selectionEnd = start + 1;
+	    		this.selectionStart = this.selectionEnd = start + 1;
 			// prevent the focus lose
-	    	e.preventDefault();
+	    		e.preventDefault();
 	   	}	
 	},false);
 });
